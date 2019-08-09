101133f20f461609ca115395eb13459add9257fd
Legaeldan <legaeldan@gmail.com>
2019-08-09 15:38:06 +0100

    Added modal to RSVP contact form.

--- assets/js/rsvp.js
+++ assets/js/rsvp.js
@@ -1,4 +1,7 @@
-
+var going = "Thank you for your response! It's great to hear you are going to be at our wedding! We look forward to it, and hope you enjoy the day!";
+var notGoing = "It's a shame you can't go. We will really miss you on this special day. We will hopefully catch up with you before then.";
+var maybe = "Maybe? MAYBE?! We haven't time for MAYBE! Get your act together!!";
+const modalResponse = document.getElementById("modalBody");
 
 function sendMail(RSVPForm) {
     emailjs.send("gmail", "test", {
@@ -11,7 +14,18 @@ function sendMail(RSVPForm) {
     .then(
         function(response) {
             console.log("SUCCESS", response);
-            alert
+            if (RSVPForm.inlineRadioOptions.value == "Going!") {
+                modalResponse.innerHTML = going;
+                $("#goingResponse").modal();
+            }
+            else if (RSVPForm.inlineRadioOptions.value == "Not Going")
+            {
+                modalResponse.innerHTML = notGoing;
+                $("#goingResponse").modal();
+            } else {
+                modalResponse.innerHTML = maybe;
+                $("#goingResponse").modal();
+            }
         },
         function(error) {
             console.log("FAILED", error);
@@ -19,3 +33,11 @@ function sendMail(RSVPForm) {
     );
     return false;  // To block from loading a new page
 };
+
+function refreshPage(){
+    window.location.reload();
+} ;
+
+$("#modalClose").on("click", function() {
+    refreshPage();
+});
